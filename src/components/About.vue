37e87a157253ee27bbc4712092242c7544b6f875
Gabriel Krieshok <gabriel.krieshok@gmail.com>
2019-10-07 14:41:30 -0400

    Updated about language

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,7 +10,8 @@
           <div>
             <p>MECVision is a Progressive Web App (PWA) that uses computer vision (object detection) to estimate the number of mosquito eggs on ovitrap paper.</p>
             <p>Check out the <a href="https://github.com/abtassociates/mecvision">project page</a> if you would like to contribute to the MECVision project or adapt it for your own purposes.</p>
-            <p>This MECVision project was developed by <a href="https://www.abtassociates.com">Abt Associates</a> with support from the <a href="https://www.usaid.gov">United States Agency for International Development (USAID)</a>'s <a href="https://www.usaid.gov/documents/1862/zika-airs-project-zap">Zika AIRS Project (ZAP)</a>.</p>
+            <p>MECVision was aided in being made possible by the generous support of the American people through the <a href="https://www.usaid.gov">United States Agency for International Development (USAID)</a> and is implemented under the <a href="https://www.usaid.gov/documents/1862/zika-airs-project-zap">Zika AIRS Project</a>.</p>
+            <p>The information on this website and web app does not necessarily represent the views of USAID or the United States Government.</p>
 
           </div>
         </div>
